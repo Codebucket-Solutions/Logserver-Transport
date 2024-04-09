e7f73f6bd7c1bedaa3012019d4eb9ad502322519
Abhinav Gautam <abhinav.nanhey@gmail.com>
2024-04-09 11:01:33 +0530

    added files

---
+++ dist/index.js
@@ -0,0 +1,89 @@
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const winston_transport_1 = __importDefault(require("winston-transport"));
+const triple_beam_1 = require("triple-beam");
+const axios_1 = __importDefault(require("axios"));
+module.exports = class LogstashTransport extends winston_transport_1.default {
+    constructor(opts) {
+        super(opts);
+        this.application = opts.application;
+        this.environment = opts.environment;
+        this.service = opts.service;
+        this.host = opts.host;
+        this.version = opts.version;
+        this.logEndpoint = opts.apiLogEndpoint ? opts.apiLogEndpoint : "logs";
+        this.batchCount = opts.batchCount ? opts.batchCount : 20;
+        this.batchInterval = opts.batchInterval ? opts.batchInterval : 5000;
+        this.axiosInstance = axios_1.default.create({
+            baseURL: opts.apiBaseUrl,
+            headers: { "x-api-key": opts.apiKey },
+        });
+        this.batchOptions = [];
+        this.batchTimeoutID = 0;
+        this.batchCallback = () => { };
+    }
+    _logTransform(info) {
+        return {
+            timestamp: info.timestamp ? info.timestamp : new Date().toISOString(),
+            version: info.version ? info.version : this.version,
+            service: info.service ? info.service : this.service,
+            application: info.application ? info.application : this.application,
+            environment: info.environment ? info.environment : this.environment,
+            logLevel: info[triple_beam_1.LEVEL],
+            host: info.host ? info.host : this.host,
+            message: info[triple_beam_1.MESSAGE],
+        };
+    }
+    log(info, callback) {
+        this._doBatch(this._logTransform(info), (options) => {
+            if (options.error) {
+                this.emit("warn", options.error.message);
+            }
+            else {
+                this.emit("logged", info);
+            }
+        });
+        if (callback) {
+            setImmediate(callback);
+        }
+    }
+    _doBatch(options, callback) {
+        this.batchOptions.push(options);
+        if (this.batchOptions.length === 1) {
+            // First message stored, it's time to start the timeout!
+            const me = this;
+            this.batchCallback = callback;
+            this.batchTimeoutID = setTimeout(function () {
+                // timeout is reached, send all messages to endpoint
+                me.batchTimeoutID = -1;
+                me._doBatchRequest(me.batchCallback);
+            }, this.batchInterval);
+        }
+        if (this.batchOptions.length === this.batchCount) {
+            // max batch count is reached, send all messages to endpoint
+            this._doBatchRequest(this.batchCallback);
+        }
+    }
+    _doBatchRequest(callback) {
+        if (this.batchTimeoutID) {
+            clearTimeout(this.batchTimeoutID);
+            this.batchTimeoutID = -1;
+        }
+        const batchOptionsCopy = this.batchOptions.slice();
+        this.batchOptions = [];
+        this._doRequest(batchOptionsCopy, callback);
+    }
+    _doRequest(options, callback) {
+        this.axiosInstance
+            .post(this.logEndpoint, { logs: options })
+            .then(function (response) {
+            callback({ response });
+        })
+            .catch(function (error) {
+            callback({ error });
+        });
+    }
+};
---
+++ src/index.ts
@@ -0,0 +1,121 @@
+import Transport from "winston-transport";
+import {
+  LEVEL, //Sabke niklenge
+  MESSAGE,
+} from "triple-beam";
+import { CustomOptions } from "./types";
+import axios, { Axios, AxiosError, AxiosResponse } from "axios";
+
+type callbackType = {
+  response?: AxiosResponse;
+  error?: AxiosError;
+};
+
+type logType = {
+  timestamp?: string;
+  version?: string | number;
+  service?: string;
+  application?: string;
+  environment?: string;
+  logLevel?: string;
+  host?: string;
+  message?: string;
+};
+
+module.exports = class LogstashTransport extends Transport {
+  private axiosInstance: Axios;
+  private application?: string;
+  private environment?: string;
+  private service?: string;
+  private host?: string;
+  private version?: string | number;
+  private logEndpoint: string;
+  private batchInterval: number;
+  private batchCount: number;
+  private batchOptions: logType[];
+  private batchTimeoutID: ReturnType<typeof setTimeout> | number;
+  private batchCallback: (options: callbackType) => void;
+  constructor(opts: CustomOptions) {
+    super(opts);
+    this.application = opts.application;
+    this.environment = opts.environment;
+    this.service = opts.service;
+    this.host = opts.host;
+    this.version = opts.version;
+    this.logEndpoint = opts.apiLogEndpoint ? opts.apiLogEndpoint : "logs";
+    this.batchCount = opts.batchCount ? opts.batchCount : 20;
+    this.batchInterval = opts.batchInterval ? opts.batchInterval : 5000;
+    this.axiosInstance = axios.create({
+      baseURL: opts.apiBaseUrl,
+      headers: { "x-api-key": opts.apiKey },
+    });
+    this.batchOptions = [];
+    this.batchTimeoutID = 0;
+    this.batchCallback = () => {};
+  }
+
+  _logTransform(info: any): logType {
+    return {
+      timestamp: info.timestamp ? info.timestamp : new Date().toISOString(),
+      version: info.version ? info.version : this.version,
+      service: info.service ? info.service : this.service,
+      application: info.application ? info.application : this.application,
+      environment: info.environment ? info.environment : this.environment,
+      logLevel: info[LEVEL],
+      host: info.host ? info.host : this.host,
+      message: info[MESSAGE],
+    };
+  }
+
+  log(info: any, callback?: () => void) {
+    this._doBatch(this._logTransform(info), (options: callbackType) => {
+      if (options.error) {
+        this.emit("warn", options.error.message);
+      } else {
+        this.emit("logged", info);
+      }
+    });
+    if (callback) {
+      setImmediate(callback);
+    }
+  }
+
+  _doBatch(options: logType, callback: (options: callbackType) => void) {
+    this.batchOptions.push(options);
+    if (this.batchOptions.length === 1) {
+      // First message stored, it's time to start the timeout!
+      const me = this;
+      this.batchCallback = callback;
+      this.batchTimeoutID = setTimeout(function () {
+        // timeout is reached, send all messages to endpoint
+        me.batchTimeoutID = -1;
+        me._doBatchRequest(me.batchCallback);
+      }, this.batchInterval);
+    }
+    if (this.batchOptions.length === this.batchCount) {
+      // max batch count is reached, send all messages to endpoint
+      this._doBatchRequest(this.batchCallback);
+    }
+  }
+
+  _doBatchRequest(callback: (options: callbackType) => void) {
+    if (this.batchTimeoutID) {
+      clearTimeout(this.batchTimeoutID);
+      this.batchTimeoutID = -1;
+    }
+    const batchOptionsCopy = this.batchOptions.slice();
+    this.batchOptions = [];
+    this._doRequest(batchOptionsCopy, callback);
+  }
+
+  _doRequest(options: logType[], callback: (options: callbackType) => void) {
+    this.axiosInstance
+      .post(this.logEndpoint, { logs: options })
+      .then(function (response: AxiosResponse) {
+        callback({ response });
+      })
+      .catch(function (error: AxiosError) {
+        callback({ error });
+      });
+  }
+};
